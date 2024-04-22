d34caec9588939fdbd685461f2bb93969823d194
s0601126 <161266658+s0601126@users.noreply.github.com>
2024-04-22 08:18:19 +0800

    Add files via upload

---
+++ DBTutorial/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace DBTutorial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstBox1 = new System.Windows.Forms.ListBox();
+            this.lstBox2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lstBox1
+            // 
+            this.lstBox1.FormattingEnabled = true;
+            this.lstBox1.ItemHeight = 12;
+            this.lstBox1.Location = new System.Drawing.Point(301, 34);
+            this.lstBox1.Name = "lstBox1";
+            this.lstBox1.Size = new System.Drawing.Size(205, 388);
+            this.lstBox1.TabIndex = 0;
+            // 
+            // lstBox2
+            // 
+            this.lstBox2.FormattingEnabled = true;
+            this.lstBox2.ItemHeight = 12;
+            this.lstBox2.Location = new System.Drawing.Point(12, 34);
+            this.lstBox2.Name = "lstBox2";
+            this.lstBox2.Size = new System.Drawing.Size(205, 388);
+            this.lstBox2.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(917, 588);
+            this.Controls.Add(this.lstBox2);
+            this.Controls.Add(this.lstBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstBox1;
+        private System.Windows.Forms.ListBox lstBox2;
+    }
+}
+
---
+++ DBTutorial/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBTutorial
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (var classicContext = new classicmodelsEntities())
+            {
+                lstBox1.Items.Clear();
+                var data = from list in classicContext.orderdetails
+                           group list by list.orderNumber into g
+                           select new { g.FirstOrDefault().orderNumber, priceSum = g.Sum(i => i.priceEach) }
+
+                    foreach (var orderNum in data.ToList())
+                {
+                    lstBox1.Items.Add(orderNum.orderNumber + " " + orderNum.priceSum);
+                }
+
+                var data1 = classicContext.orderdetails.GroupBy(i => i.orderNumber).Select(g => new
+                {
+                    orderNum = g.FirstOrDefault().orderNumber,
+                    priceSum = g.Sum(i => i.priceEach)
+                });
+
+                foreach (var orderNum in data1.ToList())
+                {
+                    lstBox2.Items.Add(orderNum.orderNum + "= " + orderNum.priceSum);
+                }
+                
+                var orderDetail = (from list in classicContext.orderdetails
+                                   select list).FirstOrDefault();
+                MessageBox.Show("Order:" + orderDetail.orders.orderNumber + " / Status:" + orderDetail.orders.status);
+
+                var data2 = classicContext.orders.Select(i => i).OrderBy(i => i.orderDate).Take(2);
+                foreach (var order in data2.ToList())
+                {
+                    foreach (var orderdetail in order.orderdetails.ToList())
+                    {
+                        MessageBox.Show("Orderline num: " + orderdetail.orderLineNumber, order.orderDate + "");
+                    }
+                }
+
+
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
